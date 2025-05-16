c4ab9281b0340bf643a8abe43c4767eb78935531
Joao Paulo Costeira <47594182+jpcosteira@users.noreply.github.com>
2025-05-16 19:46:49 +0100

    "text embedding"

---
+++ textEmbedding/src/codigo.py
@@ -0,0 +1,32 @@
+import io
+from scipy.io import loadmat, savemat
+import numpy as np
+import cv2
+
+
+def run_codigo(datafile):
+    """
+    Reads all variables from a MATLAB .mat file given a file pointer,
+    
+    Parameters:
+    file_pointer (file-like object): Opened .mat file in binary read mode.
+
+    Returns:
+    new_file_pointer (io.BytesIO): In-memory file-like object containing the cloned .mat file.
+    """
+    #Load the mat file using scipy.io.loadmat
+    mat_data=loadmat(io.BytesIO(datafile))
+
+    # SPECIFIC CODE STARTS HERE
+    im2=mat_data["im"]
+
+
+
+
+    # SPECIFIC CODE ENDS HERE
+
+    f=io.BytesIO()
+    # WRITE RETURNING DATA
+    savemat(f,{"im":im2})
+    return f.getvalue()
+
