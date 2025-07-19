8c9cb23a89e80320de5c0ff3570a76f4db097ebb
Joao Paulo Costeira <47594182+jpcosteira@users.noreply.github.com>
2025-07-19 21:52:49 +0100

    Initial Commit

--- yologpt/test/yolotest.py
+++ yologpt/test/yolotest.py
@@ -2,14 +2,22 @@ import grpc
 import yolo_pb2
 import yolo_pb2_grpc
 import cv2
+from scipy.io import savemat
 
 # Read image and encode as bytes
-img = cv2.imread("test.jpg")
-_, img_encoded = cv2.imencode('.jpg', img)
+img = cv2.imread("/home/jovyan/jpc/code/boxes/Images/eiffel.png")
+_,img_encoded = cv2.imencode('.jpg', img)
 img_bytes = img_encoded.tobytes()
 
-channel = grpc.insecure_channel('localhost:50051')
+channel = grpc.insecure_channel('Mac.lan:8061')
 stub = yolo_pb2_grpc.YOLOserviceStub(channel)
 
-response = stub.Detect(yolo_pb2.YOLORequest(image=img_bytes))
-print("YOLO Results:", response.results)
+response = stub.Detect(yolo_pb2.YOLORequest(image=img_bytes,confidence_threshold=0.2))
+
+
+with open("output.jpg","wb") as f:
+    f.write(response.labeled_image)
+detections=eval(response.detections_json)
+savemat("detections.mat",{"detections":detections})
+
+channel.close()
